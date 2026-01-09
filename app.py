6e1572525289b21b3e43409d9f7e7285a11df03a
E3A59 <john8426123@icloud.com>
2026-01-09 19:08:35 +0800

    Fix AI blocking issue

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import eventlet
+eventlet.monkey_patch()
 from flask import Flask, render_template, request, Response, send_file
 from flask_socketio import SocketIO, emit
 from threading import Thread
